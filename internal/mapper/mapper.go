a1751e65a83ec7cfe5895d64757bbe60eb5f9002
Nicolas Martin <nicolas.martin@punchh.com>
2019-06-14 09:26:21 -0400

    fix time.Time to proto timestamp conversion

--- internal/mapper/mapper.go
+++ internal/mapper/mapper.go
@@ -1,6 +1,10 @@
 package mapper
 
 import (
+	"reflect"
+
+	"github.com/golang/protobuf/ptypes"
+	"github.com/golang/protobuf/ptypes/timestamp"
 	"github.com/nmartinpunchh/banshee/internal/models"
 	journeypb "github.com/nmartinpunchh/banshee/pb/punchh/journey"
 	mapper "github.com/nmartinpunchh/go-automapper"
@@ -14,6 +18,8 @@ var journeyMapper = mapper.Mapper{
 
 // JourneyPbToModel maps journeypb to a model
 func JourneyPbToModel(jpb *journeypb.Journey) *models.Journey {
+	journeyMapper.CustomMappers = []mapper.CustomFieldMapper{pbstatus2mStatus}
+	journeyMapper.IgnoreDestFields = []string{"Status", "Model", "SegmentID", "Workflow"}
 	mJourney := &models.Journey{}
 	ret := journeyMapper.Map(jpb, mJourney)
 	if len(ret.Errors) > 0 {
@@ -23,4 +29,22 @@ func JourneyPbToModel(jpb *journeypb.Journey) *models.Journey {
 
 	return mJourney
 
+}
+
+func pbstatus2mStatus(sourceVal reflect.Value, sourceType reflect.Type, destVal reflect.Value, destType reflect.Type) (handled bool) {
+	if destType.Kind() == reflect.Struct {
+		if sourceVal.Type().Name() == "Timestamp" && destVal.Type().Name() == "Time" {
+			if sourceType.Kind() == reflect.Struct {
+				convertToIface := sourceVal.Interface().(timestamp.Timestamp)
+				val, err := ptypes.Timestamp(&convertToIface)
+				if err != nil {
+					return false
+
+				}
+				destVal.Set(reflect.ValueOf(val))
+				return true
+			}
+		}
+	}
+	return false
 }
--- internal/mapper/mapper_test.go
+++ internal/mapper/mapper_test.go
@@ -3,18 +3,26 @@ package mapper
 import (
 	"reflect"
 	"testing"
+	"time"
 
+	"github.com/golang/protobuf/ptypes"
 	"github.com/nmartinpunchh/banshee/internal/models"
 	journeypb "github.com/nmartinpunchh/banshee/pb/punchh/journey"
 )
 
 func TestJourneyPbToModel(t *testing.T) {
+	now := time.Now()
+	pbStartTime, _ := ptypes.TimestampProto(now)
 	jpb1 := &journeypb.Journey{
 		SegmentId: "sdf",
+		StartTime: pbStartTime,
+		EndTime:   pbStartTime,
 	}
 
 	jm := &models.Journey{
 		SegmentID: "sdf",
+		StartTime: now,
+		EndTime:   now,
 	}
 
 	type args struct {
